b4c0b5fc571fde86d61f983be2395540a48d79ac
Patryk Baszak <patryk.baszak@gmail.com>
2026-05-26 18:56:54 +0200

    fix: removed ast definitions from json grammar

--- packages/parser/src/Infrastructure/Grammar/Definition/Json/JsonRfc8259.php
+++ packages/parser/src/Infrastructure/Grammar/Definition/Json/JsonRfc8259.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace PhpArchitecture\Parser\Infrastructure\Grammar\Definition\Json;
 
-use PhpArchitecture\Parser\Foundation\Grammar\Definition\Definition;
 use PhpArchitecture\Parser\Foundation\Grammar\Definition\Grammar;
-use PhpArchitecture\Parser\Foundation\Grammar\Definition\Middleware\AddRuleMiddleware;
 use PhpArchitecture\Parser\Foundation\Grammar\Definition\Region;
 use PhpArchitecture\Parser\Foundation\Grammar\Definition\Rule;
 use PhpArchitecture\Parser\Infrastructure\Grammar\Definition\Technical\Whitespace;
@@ -36,31 +34,21 @@ class JsonRfc8259 extends Whitespace
                 ->closeWith(
                     Rule::token("endArray", "]", type: NodeType::Structure),
                 )
-                ->addTag("value")
-                ->asAstNode(
-                    'Array',
-                    Definition::child('Item', 'item', optional: true),
-                ),
+                ->addTag("value"),
             Rule::token("beginObject", "{", type: NodeType::Structure)
                 ->startRegion('object')
                 ->enableInheritanceFromGlobal()
                 ->add(
                     Rule::token("colon", ":", type: NodeType::Structure),
                     Rule::token("comma", ",", type: NodeType::Structure),
-                    Rule::seq("member", "string[identifier] -* colon -* value")
-                        ->asAstNode('Member'),
+                    Rule::seq("member", "string[identifier] -* colon -* value"),
                 )
                 ->withRootSequence("beginObject -* ?(member (-* comma -* member)*)/g -* endObject")
                 ->closeWith(
                     Rule::token("endObject", "}", type: NodeType::Structure),
                 )
-                ->addTag("value")
-                ->asAstNode(
-                    'Object',
-                    Definition::child('Member', 'member', optional: true),
-                ),
-            Rule::choice("primitive", ["false", "null", "true", "number", "string"], tags: ["value"])
-                ->asAstNode('Primitive'),
+                ->addTag("value"),
+            Rule::choice("primitive", ["false", "null", "true", "number", "string"], tags: ["value"]),
             Rule::keyword("null"),
             Rule::keyword("false"),
             Rule::keyword("true"),
@@ -86,18 +74,17 @@ class JsonRfc8259 extends Whitespace
             Rule::taggedWith("_number_part")
                 ->startRegion("number", true)
                 ->add(
-                    $this->addNodeTypeSetupForRules(NodeType::Raw),
                     Rule::token("decimalPoint", ".", tags: ["_number_part"]),
                     Rule::token("plus", "+", tags: ["_number_part"]),
                     Rule::token("minus", "-", tags: ["_number_part"]),
                     Rule::token("zero", "0", tags: ["_number_part"]),
                     Rule::expr("digit19", "[1-9]", tags: ["_number_part"]),
                     Rule::expr("e", "[eE]", tags: ["_number_part"]),
-                    Rule::seq("digit", "zero|digit19"),
-                    Rule::seq("digit19Seq", "digit19 digit*"),
-                    Rule::seq("exp", "e ?minus|plus digit+"),
-                    Rule::seq("integer", "zero|digit19Seq"),
-                    Rule::seq("frac", "decimalPoint digit+"),
+                    Rule::seq("digit", "zero|digit19", type: NodeType::Raw),
+                    Rule::seq("digit19Seq", "digit19 digit*", type: NodeType::Raw),
+                    Rule::seq("exp", "e ?minus|plus digit+", type: NodeType::Raw),
+                    Rule::seq("integer", "zero|digit19Seq", type: NodeType::Raw),
+                    Rule::seq("frac", "decimalPoint digit+", type: NodeType::Raw),
                 )
                 ->withRootSequence("?minus integer ?frac ?exp")
                 ->setNodeType(NodeType::Raw)
@@ -107,15 +94,5 @@ class JsonRfc8259 extends Whitespace
         $grammar->setRootRegion($jsonText);
 
         return $grammar;
-    }
-
-    private function addNodeTypeSetupForRules(NodeType $nodeType): AddRuleMiddleware
-    {
-        return AddRuleMiddleware::fromCallable(
-            static function (Rule $rule) use ($nodeType): Rule {
-                $rule->setNodeType($nodeType);
-                return $rule;
-            },
-        );
     }
 }
